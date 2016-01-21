ca701fabbd45030d15899c0f931f8f241b2f9ea8
Niall <niallpatrickmurphy@gmail.com>
2016-01-21 17:25:59 -0500

    editting views to include true paperclip functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,14 +13,10 @@ class UsersController < ApplicationController
   	redirect_to users_path
   end
 
-  def show
-  	@user = User.find(params[:id])
-  end
-
-    private
+  private
 
   def user_params
-    params.require(:user).permit(:username, :password, :fname, :lname)
+    params.require(:user).permit(:username, :password, :fname, :lname, :avatar)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates_confirmation_of :password
 	validates_presence_of :password, on: :create
+
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missingno.png"
+	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
